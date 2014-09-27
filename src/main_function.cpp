ab2458ea2a9f7e5071536f778cb877a00cc78eca
chaitanyanettem <chaitanyanettem@gmail.com>
2014-09-27 17:14:43 -0400

    updates to main_function based on new design

--- src/main_function.cpp
+++ src/main_function.cpp
@@ -1,70 +1,71 @@
 /**
- * @cnettem_assignment1
- * @author  Chaitanya Choudhary Nettem <cnettem@buffalo.edu>
- * @version 1.0
- *
- * @section LICENSE
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @section DESCRIPTION
- *
- * This contains the main function. Add further description here....
+ This file contains the main function
  */
-#include "../include/global.h"
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+#include "../include/server.h"
+#include "../include/client.h"
 
-/**
- * main function
- *
- * @param  argc Number of arguments
- * @param  argv The argument list
- * @return 0 EXIT_SUCCESS
- */
+using namespace std;
 
+//Server listening port, global variable shared with server and client files (better alternative than passing server_port to functions)
+const char* server_port;
 
-int main(int argc, char **argv)
-{
-	/*Start Here*/
+int main(int argc, char* argv []){
+
+	//Server object to start listening to the port.
+
+	if(argc!=3){
+		std::cout<<"Usage: sys <s or c> <port_no> \n";
+		exit(1);
+	}
+
+	//2nd argument: the listening port.
+	server_port=argv[2];
+
+	//If the first argument is s then the program will run as server.
+	if(!strcmp(argv[1],"s")){
+		server_operations server;
+
+		//setup the server at the listening port.
+		int sockfd=server.server_setup(argv[2]);
+
+		if (sockfd<=2){
+			exit(1);
+		}
+
+		//make the port non blocking
+		//used epoll interface for asynchronous I/O,
+		server.make_socket_non_blocking (sockfd);
+
+		//listen to requests on the server socket.
+		server.listen_to_requests(sockfd);
+	}
+
+	//If the first argument is c then the program will run as client.
+	else if(!strcmp(argv[1],"c")){
+
+		client_operations client;
+
+		//setup the server at the listening port.
+		int sockfd=client.server_setup(argv[2]);
+		if (sockfd<=2){
+			exit(1);
+		}
+		//make the port non blocking
+		client.make_socket_non_blocking (sockfd);
+		//listen to requests on the server socket.
+		client.listen_to_requests(sockfd);
+
+	}
+
+	else{
+		exit(1);
+	}
+
 
 	return 0;
-}
 
-void creator() {
-	std::cout<<"Chaitanya Choudhary Nettem\ncnettem\ncnettem@buffalo.edu";
-	std::cout<<"I have read and understood the course academic integrity policy located at\
-	 http://www.cse.buffalo.edu/faculty/dimitrio/courses/cse4589_f14/index.html#integrity";
-}
-
-void myip() {
-	//TO-DO: Find way to get external IP.
-	std::cout<<"This isn't the ip address you are looking for.";
-}
-
-void myport() {
-	//TO-DO: save port number somewhere and show it here.
-	std::cout<<"This isn't the port number you are looking for.";
-}
-
-void help(int server_or_client) {
-	//TO-DO: Complete the help messages.
-	switch(server_or_client) {
-		
-		case 0: // Server
-			std::cout<<"CREATOR - Gives creator's information\nHELP - Displays this message\nMYIP\
-			 - Displays the IP of the process\nMYPORT - Displays the port number of this process\
-			 \n";
-			break;
-		
-		case 1: //Client
-			std::cout<<"CREATOR";
-	}
 }
